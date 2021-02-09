3b7e70a368610684a167a6ba9555bc1234aabf92
matm2491 <matm2491@gmail.com>
2021-02-08 22:17:27 -0400

    v1

---
+++ cv/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    
+    return render(request, 'cv/index.html')
+
+
+
+
+def vitae(request):
+
+    return render(request, 'cv/vitae.html')
